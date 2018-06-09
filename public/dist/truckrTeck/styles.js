04b8af38b820b2f64cc97871462d206a42a61027
Amaka Nwosu <nwosuchiamaka@gmail.com>
2018-06-09 00:58:22 +0000

    modifies landing page

--- public/dist/truckrTeck/styles.js
+++ public/dist/truckrTeck/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/amaka/truckrdev/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/amaka/newtruckr/public/src/styles.css */"./src/styles.css");
 
 
 /***/ })
